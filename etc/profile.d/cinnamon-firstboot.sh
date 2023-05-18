203dcc26ad2731171df168c38f70b6d34a7864ac
A Lovett <whomever23@gmail.com>
2023-05-18 19:55:39 -0400

    feat: added vs code keybindings

--- etc/profile.d/cinnamon-firstboot.sh
+++ etc/profile.d/cinnamon-firstboot.sh
@@ -1,6 +1,6 @@
 if test "$(id -u)" -gt "0" && test -d "$HOME"; then
     if test ! -e "$HOME"/.config/autostart/cinnamon-firstboot.desktop; then
-        rsync -au /usr/share/ublue-os/firstboot/home/ "$HOME"/
+        rsync -a /usr/share/ublue-os/firstboot/home/ "$HOME"/
         customize-cinnamon
     fi
 fi
