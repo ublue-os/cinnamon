be7a2d435b1cee0484e5a42d62381f5126dc5bc0
A Lovett <whomever23@gmail.com>
2023-05-21 20:42:58 -0400

    feat: added workspace grid support

--- etc/profile.d/cinnamon-firstboot.sh
+++ etc/profile.d/cinnamon-firstboot.sh
@@ -1,6 +1,7 @@
 if test "$(id -u)" -gt "0" && test -d "$HOME"; then
     if test ! -e "$HOME"/.config/autostart/cinnamon-firstboot.desktop; then
         rsync -a /usr/share/ublue-os/firstboot/home/ "$HOME"/
+        echo  '[ $GNOME_TERMINAL_SCREEN ] && open-workspace' >> ~/.bashrc 
         customize-cinnamon
     fi
 fi
